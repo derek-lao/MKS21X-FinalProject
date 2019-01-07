c50279723442649e8d51abb33e7250fc5e1f5ff2
Derek Lao <dlao00@stuy.edu>
2019-01-07 09:59:22 -0500

    completed Square class, still need to test

--- Square.java
+++ Square.java
@@ -1,3 +1,11 @@
 public class Square{
-  int xcor, ycor
+  int xcor, ycor;
+  boolean colorRed, occupied;
+
+  public Square(int x,int y,boolean red,boolean full){
+    xcor=x;
+    ycor=y;
+    colorRed=red;
+    occupied=full;
+  }
 }
