0332a6de3e622ace015576ca980ea6e323b0cf6f
Derek Lao <dlao00@stuy.edu>
2019-01-07 13:16:06 -0500

    added constructor for Board.java, to make 8 by 8 array of Squares. And wrote a main method to test that the array of squares is actually 8 by 8

---
+++ Board.java
@@ -0,0 +1,15 @@
+public class Board{
+  private Square[][] field;
+
+  public Board(){
+    field=new Square[8][8];
+  }
+
+  public static void main(String[] args){
+    // // testing to make sure I have 8x8 board
+    // Board a=new Board();
+    // a.field=new Square[8][8];
+    // System.out.println(a.field.length);//8
+    // System.out.println(a.field[0].length);//8
+  }
+}
--- Square.java
+++ Square.java
@@ -1,7 +1,14 @@
 public class Square{
-  int xcor, ycor;
-  boolean colorRed, occupied;
+  private int xcor, ycor;
+  private boolean colorRed, occupied;
 
+  //constructor
+  /**
+  * @param x set x coordinate
+  * @param y set y coordinate
+  * @param red set color. If true, the color is red. If false, then the color is black.
+  * @param full set occupied status. If true, then the square is occupied. If false, then the square is empty.
+  */
   public Square(int x,int y,boolean red,boolean full){
     xcor=x;
     ycor=y;
