dc008992e06fca66ce9a2cb708025c5ce0cdf204
Derek Lao <dlao00@stuy.edu>
2019-01-14 02:56:32 -0500

    more testing

--- Checkers.java
+++ Checkers.java
@@ -44,49 +44,7 @@ public class Checkers{
 		while(running){
 
 			terminal.moveCursor(x,y);
-			terminal.applyBackgroundColor(Terminal.Color.RED);//cursor background color
-			terminal.applyForegroundColor(Terminal.Color.GREEN);//cursor foreground color
-			//applySGR(a,b) for multiple modifiers (bold,blink) etc.
-      // terminal.putCharacter(' ');
-			// terminal.putCharacter(' ');
-      // terminal.applyBackgroundColor(Terminal.Color.GREEN);
-			// terminal.putCharacter(' ');
-			// terminal.putCharacter(' ');
-			// terminal.moveCursor(4,6);
-			// terminal.putCharacter(' ');
-			// terminal.putCharacter(' ');
-      // terminal.applyBackgroundColor(Terminal.Color.RED);
-			// terminal.putCharacter(' ');
-			// terminal.putCharacter(' ');
-			// terminal.applySGR(Terminal.SGR.ENTER_UNDERLINE);//underlines on bottom
-			// terminal.putCharacter('\u00a4');//puts a character in place of the cursor
-			// //terminal.putCharacter(' ');
-			// terminal.applyBackgroundColor(Terminal.Color.RED);
-			// terminal.applyForegroundColor(Terminal.Color.GREEN);
-			// terminal.applySGR(Terminal.SGR.RESET_ALL);//resets all code since the last Terminal.SGR, I think?
-      //
-      //
-      //
-			// terminal.moveCursor(4,5);
-			// terminal.applyBackgroundColor(Terminal.Color.RED);
-			// terminal.applyForegroundColor(Terminal.Color.GREEN);
-			// // terminal.applySGR(Terminal.SGR.ENTER_BOLD);
-			// terminal.putCharacter(' ');
-			// terminal.putCharacter(' ');
-      // terminal.applyBackgroundColor(Terminal.Color.GREEN);
-			// terminal.putCharacter(' ');
-			// terminal.putCharacter(' ');
-			// terminal.moveCursor(4,6);
-			// terminal.putCharacter(' ');
-			// terminal.putCharacter(' ');
-      // terminal.applyBackgroundColor(Terminal.Color.RED);
-			// terminal.putCharacter(' ');
-			// terminal.putCharacter(' ');
-			// // terminal.applyBackgroundColor(Terminal.Color.DEFAULT);
-			// // terminal.applyForegroundColor(Terminal.Color.DEFAULT);
-      //
-      // terminal.moveCursor(4,5);
-      // terminal.applyBackgroundColor(Terminal.Color.BLACK);
+
       for(int r=4;r>12;r++)
       {
         for(int c=5;c>13;c++)
