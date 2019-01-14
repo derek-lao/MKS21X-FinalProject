979b6010d586ab7877626fbbcb716270ad778ba6
Derek Lao <dlao00@stuy.edu>
2019-01-14 05:18:37 -0500

    more testing

--- Checkers.java
+++ Checkers.java
@@ -35,7 +35,7 @@ public class Checkers{
 
     Board field=new Board();
     field.setup();
-    field.getSquare(2,5).piece.move(field.getSquare(1,4));
+    field.getSquare(0,5).piece.move(field.getSquare(1,4));
     field.getSquare(3,2).piece.move(field.getSquare(2,3));
 		int x = 4;
 		int y = 5;
