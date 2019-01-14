cdc1f843d91dcdb6d8780380e1c82286a672fd75
Derek Lao <dlao00@stuy.edu>
2019-01-14 04:54:49 -0500

    more testing

--- Piece.java
+++ Piece.java
@@ -22,9 +22,9 @@ public class Piece{
   }
 
   private void motion(Square target){
-    this.setPosition(target);
+    // this.setPosition(target);
     target.setPiece(this);
-    this.position.setPiece(null);
+    // this.position.setPiece(null);
   }
   public boolean move(Square target){
     Square move1=position.getSquare1();
