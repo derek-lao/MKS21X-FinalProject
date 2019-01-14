b6bc378192d729f53eaf5d156986b2ed387d9ff2
Derek Lao <dlao00@stuy.edu>
2019-01-14 01:35:26 -0500

    commented out soviet code

--- Checkers.java
+++ Checkers.java
@@ -72,21 +72,21 @@ public class Checkers{
           }
         }
       }
-			terminal.moveCursor(4,5);
-			terminal.applyBackgroundColor(Terminal.Color.RED);
-			terminal.applyForegroundColor(Terminal.Color.YELLOW);
-			terminal.applySGR(Terminal.SGR.ENTER_BOLD);
-			terminal.putCharacter(' ');
-			terminal.putCharacter(' ');
-			terminal.putCharacter('\u262d');
-			terminal.putCharacter(' ');
-			terminal.moveCursor(size.getColumns()-5,6);
-			terminal.putCharacter(' ');
-			terminal.putCharacter(' ');
-			terminal.putCharacter(' ');
-			terminal.putCharacter(' ');
-			terminal.applyBackgroundColor(Terminal.Color.DEFAULT);
-			terminal.applyForegroundColor(Terminal.Color.DEFAULT);
+			// terminal.moveCursor(4,5);
+			// terminal.applyBackgroundColor(Terminal.Color.RED);
+			// terminal.applyForegroundColor(Terminal.Color.YELLOW);
+			// terminal.applySGR(Terminal.SGR.ENTER_BOLD);
+			// terminal.putCharacter(' ');
+			// terminal.putCharacter(' ');
+			// terminal.putCharacter('\u262d');
+			// terminal.putCharacter(' ');
+			// terminal.moveCursor(size.getColumns()-5,6);
+			// terminal.putCharacter(' ');
+			// terminal.putCharacter(' ');
+			// terminal.putCharacter(' ');
+			// terminal.putCharacter(' ');
+			// terminal.applyBackgroundColor(Terminal.Color.DEFAULT);
+			// terminal.applyForegroundColor(Terminal.Color.DEFAULT);
 
 			Key key = terminal.readInput();
 
