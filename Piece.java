f31d8bda7abf3c80c4afee941e6f980e1b63ba14
derek-lao <dlao00@stuy.edu>
2019-01-17 10:21:53 -0500

    migrated move and capture to Player class, to simplify things A LOT

--- Piece.java
+++ Piece.java
@@ -22,105 +22,105 @@ public class Piece{
   }
 
   // motion into any square, a helper method designed to help
-  private void motion(Square target){
+  public void motion(Square target){
     position.setPiece(null);
     position=target;
     target.setPiece(this);
   }
 
-  public boolean move(Square target){
-    Square move1=position.getSquare1();
-    Square move2=position.getSquare2();
-    Square move3=position.getSquare3();getSquare1
-    Square move4=position.getSquare4();
-    if(target!=null&&(target==move1||target==move2||
-    target==move3||target==move4)&&target.isOccupied()==false)
-    {
-      if(this.king)
-      {
-        this.motion(target);
-        return true;
-      }
-      else
-      {
-        if(this.colorRed&&(target==move1||target==move2))
-        {
-          this.motion(target);
-          return true;
-        }
-        if((!this.colorRed)&&(target==move3||target==move4))
-        {
-          this.motion(target);
-          return true;
-        }
-        else
-        {
-          return false;
-        }
-      }
-    }
-    else
-    {
-      return false;
-    }
-  }
-  public boolean capture(Piece captive){
-    Piece cap1=position.getSquare1().piece;
-    Piece cap2=position.getSquare2().piece;
-    Piece cap3=position.getSquare3().piece;
-    Piece cap4=position.getSquare4().piece;
-    System.out.println(position);
-    System.out.println(position.getSquare1());
-    System.out.println(position.getSquare1().getSquare1());
-    Square capture1=position.getSquare1().getSquare1();
-    Square capture2=position.getSquare2().getSquare2();
-    Square capture3=position.getSquare3().getSquare3();
-    Square capture4=position.getSquare4().getSquare4();
-    boolean status1= !capture1.isOccupied();
-    boolean status2= !capture2.isOccupied();
-    boolean status3= !capture3.isOccupied();
-    boolean status4= !capture4.isOccupied();
-    if(captive!=null&&captive.colorRed!=this.colorRed&&
-    (captive==cap1||captive==cap2||captive==cap3||captive==cap4)&&
-    (capture1!=null||capture2!=null||capture3!=null||capture4!=null)&&
-    (status1||status2||status3||status4))
-    {
-      if(this.king)
-      {
-        if(captive==cap1) this.motion(capture1);
-        if(captive==cap2) this.motion(capture2);
-        if(captive==cap3) this.motion(capture3);
-        if(captive==cap4) this.motion(capture4);
-        captive.getPosition().piece=null;
-        captive.setPosition(null);
-        return true;
-      }
-      else
-      {
-        if(colorRed&&(captive==cap1||captive==cap2))
-        {
-          if(captive==cap1) this.motion(capture1);
-          if(captive==cap2) this.motion(capture2);
-          captive.getPosition().piece=null;
-          captive.setPosition(null);
-          return true;
-        }
-        if(!colorRed&&(captive==cap3||captive==cap4))
-        {
-          if(captive==cap3) this.motion(capture3);
-          if(captive==cap4) this.motion(capture4);
-          captive.getPosition().piece=null;
-          captive.setPosition(null);
-          return true;
-        }
-        else
-        {
-          return false;
-        }
-      }
-    }
-    return false;
-  }
+  // public boolean move(Square target){
+  //   Square move1=position.getSquare1();
+  //   Square move2=position.getSquare2();
+  //   Square move3=position.getSquare3();getSquare1
+  //   Square move4=position.getSquare4();
+  //   if(target!=null&&(target==move1||target==move2||
+  //   target==move3||target==move4)&&target.isOccupied()==false)
+  //   {
+  //     if(this.king)
+  //     {
+  //       this.motion(target);
+  //       return true;
+  //     }
+  //     else
+  //     {
+  //       if(this.colorRed&&(target==move1||target==move2))
+  //       {
+  //         this.motion(target);
+  //         return true;
+  //       }
+  //       if((!this.colorRed)&&(target==move3||target==move4))
+  //       {
+  //         this.motion(target);
+  //         return true;
+  //       }
+  //       else
+  //       {
+  //         return false;
+  //       }
+  //     }
+  //   }
+  //   else
+  //   {
+  //     return false;
+  //   }
+  // }
+  // public boolean capture(Piece captive){
+  //   Piece cap1=position.getSquare1().piece;
+  //   Piece cap2=position.getSquare2().piece;
+  //   Piece cap3=position.getSquare3().piece;
+  //   Piece cap4=position.getSquare4().piece;
+  //   System.out.println(position);
+  //   System.out.println(position.getSquare1());
+  //   System.out.println(position.getSquare1().getSquare1());
+  //   Square capture1=position.getSquare1().getSquare1();
+  //   Square capture2=position.getSquare2().getSquare2();
+  //   Square capture3=position.getSquare3().getSquare3();
+  //   Square capture4=position.getSquare4().getSquare4();
+  //   boolean status1= !capture1.isOccupied();
+  //   boolean status2= !capture2.isOccupied();
+  //   boolean status3= !capture3.isOccupied();
+  //   boolean status4= !capture4.isOccupied();
+  //   if(captive!=null&&captive.colorRed!=this.colorRed&&
+  //   (captive==cap1||captive==cap2||captive==cap3||captive==cap4)&&
+  //   (capture1!=null||capture2!=null||capture3!=null||capture4!=null)&&
+  //   (status1||status2||status3||status4))
+  //   {
+  //     if(this.king)
+  //     {
+  //       if(captive==cap1) this.motion(capture1);
+  //       if(captive==cap2) this.motion(capture2);
+  //       if(captive==cap3) this.motion(capture3);
+  //       if(captive==cap4) this.motion(capture4);
+  //       captive.getPosition().piece=null;
+  //       captive.setPosition(null);
+  //       return true;
+  //     }
+  //     else
+  //     {
+  //       if(colorRed&&(captive==cap1||captive==cap2))
+  //       {
+  //         if(captive==cap1) this.motion(capture1);
+  //         if(captive==cap2) this.motion(capture2);
+  //         captive.getPosition().piece=null;
+  //         captive.setPosition(null);
+  //         return true;
+  //       }
+  //       if(!colorRed&&(captive==cap3||captive==cap4))
+  //       {
+  //         if(captive==cap3) this.motion(capture3);
+  //         if(captive==cap4) this.motion(capture4);
+  //         captive.getPosition().piece=null;
+  //         captive.setPosition(null);
+  //         return true;
+  //       }
+  //       else
+  //       {
+  //         return false;
+  //       }
+  //     }
+  //   }
+  //   return false;
+  // }
 
   public void becomeKing(){
     if(colorRed && !king && position.getY()==0) king=true;
--- Player.java
+++ Player.java
@@ -8,25 +8,91 @@ public class Player{
     turn=myTurn;
   }
 
-  public boolean move(Piece soldier, Square target){
-    if(myTurn&&soldier.colorRed==this.colorRed&&soldier.move(target))
+  public boolean move(Piece warrior,Square target){
+    if(warrior!=null && !target.isOccupied() && !target.isRed())
     {
-      return soldier.move(target);
-    }
-    else
-    {
-      return false;
-    }
-  }
-
-  public boolean capture(Piece soldier, Piece captive){
-    if(myTurn&&soldier.colorRed==this.colorRed&&soldier.capture(captive))
-    {
-      return soldier.capture(captive);
-    }
-    else
-    {
-      return false;
+      if(warrior.king)
+      {
+        
+      }
     }
   }
+  public boolean capture(Piece captive){
+    Piece cap1=position.getSquare1().piece;
+    Piece cap2=position.getSquare2().piece;
+    Piece cap3=position.getSquare3().piece;
+    Piece cap4=position.getSquare4().piece;
+    System.out.println(position);
+    System.out.println(position.getSquare1());
+    System.out.println(position.getSquare1().getSquare1());
+    Square capture1=position.getSquare1().getSquare1();
+    Square capture2=position.getSquare2().getSquare2();
+    Square capture3=position.getSquare3().getSquare3();
+    Square capture4=position.getSquare4().getSquare4();
+    boolean status1= !capture1.isOccupied();
+    boolean status2= !capture2.isOccupied();
+    boolean status3= !capture3.isOccupied();
+    boolean status4= !capture4.isOccupied();
+    if(captive!=null&&captive.colorRed!=this.colorRed&&
+    (captive==cap1||captive==cap2||captive==cap3||captive==cap4)&&
+    (capture1!=null||capture2!=null||capture3!=null||capture4!=null)&&
+    (status1||status2||status3||status4))
+    {
+      if(this.king)
+      {
+        if(captive==cap1) this.motion(capture1);
+        if(captive==cap2) this.motion(capture2);
+        if(captive==cap3) this.motion(capture3);
+        if(captive==cap4) this.motion(capture4);
+        captive.getPosition().piece=null;
+        captive.setPosition(null);
+        return true;
+      }
+      else
+      {
+        if(colorRed&&(captive==cap1||captive==cap2))
+        {
+          if(captive==cap1) this.motion(capture1);
+          if(captive==cap2) this.motion(capture2);
+          captive.getPosition().piece=null;
+          captive.setPosition(null);
+          return true;
+        }
+        if(!colorRed&&(captive==cap3||captive==cap4))
+        {
+          if(captive==cap3) this.motion(capture3);
+          if(captive==cap4) this.motion(capture4);
+          captive.getPosition().piece=null;
+          captive.setPosition(null);
+          return true;
+        }
+        else
+        {
+          return false;
+        }
+      }
+    }
+    return false;
+  }
+  // public boolean move(Piece soldier, Square target){
+  //   if(myTurn&&soldier.colorRed==this.colorRed&&soldier.move(target))
+  //   {
+  //     return soldier.move(target);
+  //   }
+  //   else
+  //   {
+  //     return false;
+  //   }
+  // }
+  //
+  // public boolean capture(Piece soldier, Piece captive){
+  //   if(myTurn&&soldier.colorRed==this.colorRed&&soldier.capture(captive))
+  //   {
+  //     return soldier.capture(captive);
+  //   }
+  //   else
+  //   {
+  //     return false;
+  //   }
+  // }
 }
