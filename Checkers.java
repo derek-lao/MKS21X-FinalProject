57efe24ccb3ff7e806620adbea3ffdbaa36486cd
Derek Lao <dlao00@stuy.edu>
2019-01-17 20:51:20 -0500

    added code to make capture. This was the line of capture that worked last time

--- Checkers.java
+++ Checkers.java
@@ -41,7 +41,7 @@ public class Checkers{
     field.setup();
     red.move(field.getSquare(4,5).piece,field.getSquare(5,4));
     black.move(field.getSquare(3,2).piece,field.getSquare(4,3));
-    // field.getSquare(5,4).piece.capture(field.getSquare(4,3).piece);
+    red.capture(field.getSquare(5,4).piece,field.getSquare(3,2));
     // field.getSquare(4,1).piece.capture(field.getSquare(3,2).piece);
     // field.getSquare(0,5).piece.capture(field.getSquare(1,4).piece);
     // field.getSquare(1,4).piece.capture(field.getSquare(2,3).piece);
