badc729ef81437f52eab69fce7e2d280f71e17ec
Derek Lao <dlao00@stuy.edu>
2019-01-21 18:50:47 -0500

    more fixing

--- Checkers.java
+++ Checkers.java
@@ -106,11 +106,11 @@ public class Checkers{
         terminal.applyBackgroundColor(Terminal.Color.BLACK);
         long tEnd = System.currentTimeMillis();
   			long millis = tEnd - tStart;
-  			putString(1,2,terminal,"Milliseconds since start of program: "+millis+"                               ");
+  			putString(1,2,terminal,"Milliseconds since start of program: "+millis+"     ");
   			if(millis/1000 > lastSecond){
   				lastSecond = millis / 1000;
   				//one second has passed.
-  				putString(1,3,terminal,"Seconds since start of program: "+lastSecond+"                              ");
+  				putString(1,3,terminal,"Seconds since start of program: "+lastSecond+"    ");
         }
 
         terminal.applyForegroundColor(Terminal.Color.YELLOW);
