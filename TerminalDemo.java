94e1c7e2fe3a2263555562cdc6206462eb83a3b1
Derek Lao <dlao00@stuy.edu>
2019-01-21 04:13:18 -0500

    more testing

--- TerminalDemo.java
+++ TerminalDemo.java
@@ -50,13 +50,13 @@ public class TerminalDemo {
 			terminal.applyBackgroundColor(Terminal.Color.RED);//cursor background color
 			terminal.applyForegroundColor(Terminal.Color.GREEN);//cursor foreground color
 			//applySGR(a,b) for multiple modifiers (bold,blink) etc.
-			terminal.applySGR(Terminal.SGR.ENTER_REVERSE );//underlines on bottom
+			// terminal.applySGR(Terminal.SGR.ENTER_REVERSE );//underlines on bottom
 			terminal.applySGR(Terminal.SGR.ENTER_UNDERLINE);
 			terminal.putCharacter('\u00a4');//puts a character in place of the cursor
 			//terminal.putCharacter(' ');
 			terminal.applyBackgroundColor(Terminal.Color.RED);
 			terminal.applyForegroundColor(Terminal.Color.GREEN);
-			// terminal.applySGR(Terminal.SGR.RESET_ALL);//resets all code since the last Terminal.SGR, I think?
+			terminal.applySGR(Terminal.SGR.RESET_ALL);//resets all code since the last Terminal.SGR, I think?
 
 
 			// terminal.moveCursor(size.getColumns()-5,5);
