7c5d69f61f1ee4f9d2673446beacf42a10c1a458
Derek Lao <dlao00@stuy.edu>
2019-01-21 03:54:40 -0500

    commented out soviet flag material, and for the readinput key, I put screen.readInput() and I pray that it works

--- TerminalDemo.java
+++ TerminalDemo.java
@@ -59,23 +59,23 @@ public class TerminalDemo {
 			terminal.applySGR(Terminal.SGR.RESET_ALL);//resets all code since the last Terminal.SGR, I think?
 
 
-			terminal.moveCursor(size.getColumns()-5,5);
-			terminal.applyBackgroundColor(Terminal.Color.RED);
-			terminal.applyForegroundColor(Terminal.Color.YELLOW);
-			terminal.applySGR(Terminal.SGR.ENTER_BOLD);
-			terminal.putCharacter(' ');
-			terminal.putCharacter(' ');
-			terminal.putCharacter('\u262d');
-			terminal.putCharacter(' ');
-			terminal.moveCursor(size.getColumns()-5,6);
-			terminal.putCharacter(' ');
-			terminal.putCharacter(' ');
-			terminal.putCharacter(' ');
-			terminal.putCharacter(' ');
+			// terminal.moveCursor(size.getColumns()-5,5);
+			// terminal.applyBackgroundColor(Terminal.Color.RED);
+			// terminal.applyForegroundColor(Terminal.Color.YELLOW);
+			// terminal.applySGR(Terminal.SGR.ENTER_BOLD);
+			// terminal.putCharacter(' ');
+			// terminal.putCharacter(' ');
+			// terminal.putCharacter('\u262d');
+			// terminal.putCharacter(' ');
+			// terminal.moveCursor(size.getColumns()-5,6);
+			// terminal.putCharacter(' ');
+			// terminal.putCharacter(' ');
+			// terminal.putCharacter(' ');
+			// terminal.putCharacter(' ');
 			// terminal.applyBackgroundColor(Terminal.Color.DEFAULT);
 			// terminal.applyForegroundColor(Terminal.Color.DEFAULT);
 
-			Key key = terminal.readInput();
+			Key key = screen.readInput();
 
 			if (key != null)
 			{
