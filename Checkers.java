db45c7a2bbb98fb56899b91b768584188a8efeac
Derek Lao <dlao00@stuy.edu>
2019-01-21 19:53:25 -0500

    I think I am done. It has been fun.

--- Checkers.java
+++ Checkers.java
@@ -47,24 +47,24 @@ public class Checkers{
 
     // set of moves for early testing
     field.setup();
-    red.move(field.getSquare(5,6).piece,field.getSquare(6,5));
-    red.myTurn=!red.myTurn;
-    black.myTurn=!black.myTurn;
-    black.move(field.getSquare(6,3).piece,field.getSquare(5,4));
-    black.move(field.getSquare(5,4).piece,field.getSquare(4,5));
-    black.move(field.getSquare(4,5).piece,field.getSquare(5,6));
-    black.move(field.getSquare(7,2).piece,field.getSquare(6,3));
-    black.move(field.getSquare(6,3).piece,field.getSquare(5,4));
-    black.move(field.getSquare(8,1).piece,field.getSquare(7,2));
-    red.myTurn=!red.myTurn;
-    black.myTurn=!black.myTurn;
-    red.capture(field.getSquare(6,7).piece,field.getSquare(4,5));
-    red.capture(field.getSquare(4,5).piece,field.getSquare(6,3));
-    red.capture(field.getSquare(6,3).piece,field.getSquare(8,1));
-    red.myTurn=!red.myTurn;
-    black.myTurn=!black.myTurn;
-    redPiecesLeft=12;
-    blackPiecesLeft=1;
+    // red.move(field.getSquare(5,6).piece,field.getSquare(6,5));
+    // red.myTurn=!red.myTurn;
+    // black.myTurn=!black.myTurn;
+    // black.move(field.getSquare(6,3).piece,field.getSquare(5,4));
+    // black.move(field.getSquare(5,4).piece,field.getSquare(4,5));
+    // black.move(field.getSquare(4,5).piece,field.getSquare(5,6));
+    // black.move(field.getSquare(7,2).piece,field.getSquare(6,3));
+    // black.move(field.getSquare(6,3).piece,field.getSquare(5,4));
+    // black.move(field.getSquare(8,1).piece,field.getSquare(7,2));
+    // red.myTurn=!red.myTurn;
+    // black.myTurn=!black.myTurn;
+    // red.capture(field.getSquare(6,7).piece,field.getSquare(4,5));
+    // red.capture(field.getSquare(4,5).piece,field.getSquare(6,3));
+    // red.capture(field.getSquare(6,3).piece,field.getSquare(8,1));
+    // red.myTurn=!red.myTurn;
+    // black.myTurn=!black.myTurn;
+    // redPiecesLeft=12;
+    // blackPiecesLeft=1;
     // black.capture(field.getSquare(5,8).piece,field.getSquare(7,6));
 
 
